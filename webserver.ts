4e8991b17f0e7e1cf6ee783a9dacd730e9cdddf6
Keisuke Nakayama <me@yammer.jp>
2021-11-14 21:02:46 +0900

    Fetch and republish RSS feed

--- webserver.ts
+++ webserver.ts
@@ -1,13 +1,18 @@
-// ref: https://deno.land/manual/examples/http_server
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 const addr = ":8080";
 
-const handler = (request: Request): Response => {
-  let body = "Your user-agent is:\n\n";
-  body += request.headers.get("user-agent") || "Unknown";
+const handler = async (request: Request): Promise<Response> => {
+  const body = await fetchRSS();
 
-  return new Response(body, { status: 200 });
+  return new Response(body, { status: 200, headers: {
+    'Content-Type': 'application/rss+xml; charset=utf-8'
+  }});
+};
+
+const fetchRSS = async (): Promise<string> => {
+  const response = await fetch('https://memo.yammer.jp/posts/index.xml');
+  return response.text();
 };
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
