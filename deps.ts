c9c3726f2658824c26e80ada75194df214513c12
Keisuke Nakayama <me@yammer.jp>
2021-11-18 10:24:12 +0900

    WIP: RSS2.0 parsing

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-export { serve }
+export { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 import { Env } from "https://deno.land/x/env@v2.2.0/env.js";
 const env = new Env();
 export { env }
+
+export { parse, stringify } from 'https://deno.land/x/xml/mod.ts';
--- main.ts
+++ main.ts
@@ -1,20 +1,48 @@
 import { serve } from './deps.ts'
 import { env } from "./deps.ts"
+import { parse, stringify } from "./deps.ts"
+const XML = { parse, stringify }
 
-const addr = `:${env.get("PORT", "8080")}`;
+import { any2RSS2Channel } from './rss2.ts'
 
-const handler = async (request: Request): Promise<Response> => {
-  const body = await fetchRSS();
-
-  return new Response(body, { status: 200, headers: {
-    'Content-Type': 'application/rss+xml; charset=utf-8'
-  }});
-};
-
-const fetchRSS = async (): Promise<string> => {
+const fetchRSSText = async (): Promise<string> => {
   const response = await fetch('https://memo.yammer.jp/posts/index.xml');
   return response.text();
 };
 
+const fetchRSS = async (): Promise<any> => {
+  const rssText = await fetchRSSText()
+  const rssTree = XML.parse(rssText)
+
+  if (
+    !('rss' in rssTree) ||
+    rssTree.rss === null || typeof rssTree.rss !== 'object' || !('channel' in rssTree.rss)
+  ) {
+    return {}
+  }
+  return await any2RSS2Channel(rssTree.rss.channel)
+}
+
+const addr = `:${env.get("PORT", "8080")}`;
+
+const handler = async (request: Request): Promise<Response> => {
+  try {
+    const body = JSON.stringify((await fetchRSS()));
+
+    return new Response(body, { status: 200, headers: {
+      // 'Content-Type': 'application/rss+xml; charset=utf-8'
+    }});
+
+  }
+  catch(e) {
+    console.error(e)
+    return new Response(e.toString(), { status: 200, headers: {
+      // 'Content-Type': 'application/rss+xml; charset=utf-8'
+    }});
+
+  }
+};
+
 console.log(`HTTP webserver running. Access it at: http://localhost${addr}/`);
 await serve(handler, { addr });
+
