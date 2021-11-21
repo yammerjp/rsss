13d75f735fae9be204942cec26d57b6695c2d01a
Keisuke Nakayama <me@yammer.jp>
2021-11-21 17:15:23 +0900

    Add CORS header for https://yammer.jp

--- main.ts
+++ main.ts
@@ -13,7 +13,14 @@ router.get('/v0', indexHandler)
 router.get('/v0/rss', rssHandler)
 router.get('/v0/json_feed', jsonFeedHandler)
 
+router.registerCommonHeader('Access-Control-Allow-Origin', 'https://yammer.jp')
+
 const addr = `:${env.get("PORT", "8080")}`;
 console.log(`HTTP webserver running. Access it at: http://localhost${addr}/`);
-await serve(req => router.handle(req), { addr });
+
+const handler = (req: Request): Promise<Response> => {
+  return router.handle(req)
+}
+
+await serve(handler, { addr });
 
