3ab111ce706d92de6902354935eaa1f432d77905
Keisuke Nakayama <me@yammer.jp>
2021-11-21 17:39:46 +0900

    Add to allow http://localhost:8080

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ router.get('/v0', indexHandler)
 router.get('/v0/rss', rssHandler)
 router.get('/v0/json_feed', jsonFeedHandler)
 router.allowOrigin('https://yammer.jp')
+router.allowOrigin('http://localhost:8080')
 router.allowOrigin('http://localhost:8081')
 
 const addr = `:${env.get("PORT", "8080")}`;
